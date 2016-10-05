62dd8879a991e84e8cf8ded8e09ef20a4f9ee3f5
nathan.vangheem <nathan.vangheem@wildcardcorp.com>
2016-10-04 23:03:46 -0500

    add basic pattern example

---
+++ collective/jstraining/exercise6/static/exercise6-compiled.js
@@ -0,0 +1,24 @@
+/* global require */
+
+require([
+  'jquery',
+  'mockup-patterns-base',
+], function($, Base) {
+  'use strict';
+
+  var Pattern = Base.extend({
+    name: 'exercise6',
+    trigger: '.pat-exercise6',
+    parser: 'mockup',
+    defaults: {
+    },
+    init: function() {
+      var that = this;
+      that.$el.append(' <span>Exercise 6 was here</span>');
+    }
+  });
+
+});
+
+define("/Users/vangheem/code/minimalplone5/src/collective.jstraining/collective/jstraining/exercise6/static/pattern.js", function(){});
+
