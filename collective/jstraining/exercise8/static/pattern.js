8a5596854b122d39856dab5effbd79f8582e5b96
nathan.vangheem <nathan.vangheem@wildcardcorp.com>
2016-10-06 23:17:21 -0500

    implement exercise 8

---
+++ collective/jstraining/exercise8/static/pattern.js
@@ -0,0 +1,25 @@
+/* global require */
+
+require([
+  'jquery',
+  'mockup-patterns-base',
+  'tablesorter'
+], function($, Base) {
+  'use strict';
+
+  /* combining bundle and pattern in same file this example */
+
+  Base.extend({
+    name: 'tablesorter',
+    trigger: '.pat-tablesorter',
+    parser: 'mockup',
+    defaults: {
+    },
+    init: function() {
+      var that = this;
+      debugger;
+      that.$el.tablesorter();
+    }
+  });
+
+});
