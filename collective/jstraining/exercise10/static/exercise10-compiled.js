012912710df03a1527ab2202ce6eb87eb281df1a
nathan.vangheem <nathan.vangheem@wildcardcorp.com>
2016-10-06 23:47:02 -0500

    implement exercise 10

---
+++ collective/jstraining/exercise10/static/exercise10-compiled.js
@@ -0,0 +1,35 @@
+/* global require */
+
+require([
+  'jquery',
+  'mockup-patterns-livesearch',
+  'pat-registry'
+], function($, Livesearch, registry) {
+  'use strict';
+  /* combining bundle and pattern in same file this example */
+
+
+  // first, unregister existing pattern
+  delete registry.patterns.livesearch;
+  delete $.fn.patLivesearch;
+
+
+  // creating new pattern automatically registers it
+  Livesearch.extend({
+    name: 'livesearch',
+    trigger: '.pat-livesearch',
+    parser: 'mockup',
+    init: function() {
+      var that = this;
+      Livesearch.prototype.init.call(that);
+
+      // all we're doing in this customization is defaulting to searching
+      // current section
+      $('.searchSection input', that.$el)[0].checked = true;
+    }
+  });
+
+});
+
+define("/Users/vangheem/code/minimalplone5/src/collective.jstraining/collective/jstraining/exercise10/static/pattern.js", function(){});
+
