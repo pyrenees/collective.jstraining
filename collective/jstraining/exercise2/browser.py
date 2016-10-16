7ad293ae39a5fbf466254cf8fa9fc7e43a06b94a
Ramon Navarro Bosch <ramon@MacBook-Air-de-Ramon.local>
2016-10-16 18:36:47 -0400

    collective/jstraining/exercise2/static/exercise2-compiled.css

--- collective/jstraining/exercise2/browser.py
+++ collective/jstraining/exercise2/browser.py
@@ -1,4 +1,4 @@
-from Products.CMFPlone.resources import add_resource_on_request
+from Products.CMFPlone.resources import add_bundle_on_request
 from Products.Five import BrowserView
 
 
@@ -6,5 +6,5 @@ class Exercise2View(BrowserView):
 
     def __call__(self):
         # utility function to add resource to rendered page
-        add_resource_on_request(self.request, 'exercise2')
+        add_bundle_on_request(self.request, 'exercise2')
         return super(Exercise2View, self).__call__()
